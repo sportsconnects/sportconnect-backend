77fec3d134eccd2bd487f0266ffe79100a98117e
jjbaah068 <jjbaah068@gmail.com>
2026-03-10 20:05:23 +0000

    updated the cors error issue

--- src/server.js
+++ src/server.js
@@ -16,7 +16,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({
  origin: [
-    "http://localhost:5173",                   
+    "http://localhost:5173",
+    "http://localhost:5174",                   
     "https://sportsconnectz.netlify.app"        
   ],
   credentials: true
