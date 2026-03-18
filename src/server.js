0d596a91d8a3191d22adb2747e3395eaa1e836f8
jjbaah068 <jjbaah068@gmail.com>
2026-03-18 19:34:38 +0000

    updated listen for the server

--- src/server.js
+++ src/server.js
@@ -113,6 +113,9 @@ if (process.env.NODE_ENV === "production" && process.env.RENDER_EXTERNAL_URL) {
 
 // ── Start server 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
